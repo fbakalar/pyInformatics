6c3c9e18a16332dd635f646604d85710e9f335cf
fbakalar <berniebakalar@yahoo.com>
2016-11-13 21:40:16 -0600

    added randint and choice examples

--- Chapter4/ch4random.py
+++ Chapter4/ch4random.py
@@ -5,3 +5,23 @@ import random
 for i in range(10):
     x = random.random()
     print(x)
+
+
+# randint
+#  \n = new line
+print("\n")
+print("now we'll use randint")
+
+for i in range(10):
+    x = random.randint(1, 100)
+    print(x)
+
+
+# choice
+print("\n")
+print("using choice of {a,b,c,d,e,f,g}")
+
+t = ['a','b','c','d','e','f','g']
+for i in range(10):
+    x = random.choice(t)
+    print(x)
