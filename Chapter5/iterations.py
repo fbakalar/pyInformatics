d0968d6fed418bd326ead848f497d233e542625b
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-11-16 22:22:11 -0600

    added for loop

--- Chapter5/iterations.py
+++ Chapter5/iterations.py
@@ -4,10 +4,15 @@
 #
 # -------- Looping Patterns Keywords -----------
 #
-#      While vs IF
-#      Continue
-#      For
+#      
+#      while vs if
+#        - a while is an 'indefinite' loop
+#      continue
+#      for
+#        - a for is a 'definite' loop
+#        - executes through a definite set
 #      True
+#      break
 #
 ###################################################################
 
@@ -45,6 +50,28 @@ while True:
     line = raw_input('>')
     if line == 'done':
         break
+    print line  # this line does not execute if break
+print 'Done!'
+
+#------------------------
+# the <continue> statement ends the current iteration and jumps to the 
+# top of the loop and starts the next iteration
+# don't stop this loop - advance to the next iteration
+# so pattern can be - while True - if...continue - else - finish loop via if.. break
+# continue skips the second if
+
+while True:
+    line = raw_input('>')
+    if line[0] == '#':
+        continue
+    if line == 'done':
+        break
     print line
 print 'Done!'
 
+#---------------------------
+
+for i in [5,4,3,2,1]:
+    print i
+
+print 'Blastoff!'
