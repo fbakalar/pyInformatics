c086213035ea1b431c74a25f8fa9bf0c78002fd6
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-11-16 21:53:35 -0600

    First Chapter 5 commit

---
+++ Chapter5/iterations.py
@@ -0,0 +1,21 @@
+###################################################################
+#
+#  file holds examples from Chapter 5 of Python Informatics
+#
+# -------- Looping Patterns Keywords -----------
+#
+#      While vs IF
+#      Continue
+#      For
+#
+###################################################################
+
+n = 25
+x = 1
+
+while x > n:
+    print n
+    x = x+1
+
+print 'Blastoff!'
+print n
