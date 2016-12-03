6c967560467fb258aee24f33d9c8aec09d345c44
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-12-02 21:11:12 -0600

    loop pattens

--- Chapter5/loopPatterns.py
+++ Chapter5/loopPatterns.py
@@ -9,13 +9,20 @@
 
 # counting and summing
 
+# Note Python has a built in function to count a list
+# len()
+
 count = 0
 for itervar in [3,41,12,9,74,15]:
     count = count + 1
 print 'Count:   ',  count
 
 
+
+#  Note Python has a built in function to sum a list
+#   sum()
+
 total = 0
 for itervar in [3,41,12,9, 74, 15]:
-    total = total + itervar
+    total = total + itervar     # sometimes variable is caaled an accumulator
 print 'Total: ', total
